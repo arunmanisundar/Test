fd2153f53711fe8db427b8d00172d6202d474351
arunmanisundar <mspisces3@gmail.com>
2014-04-24 20:52:46 +0530

    added html
    ist change

---
+++ footer.php
@@ -0,0 +1,64 @@
+<!-- Footer -->
+	<div id="footer" class="container">
+		<div class="row footer_inside">
+		  <div class="four columns">
+		    <h3>About Us</h3>
+		    <p>IN4 Solution was set up in the year 2006, as a proprietary concern with the prime objective of developing and giving solutions for Attendance Management System.Soon IN4 solution became a trusted name in Time Management and Access Control Systems as the solutions and services we provided were far beyond the customer's requirements.</p>
+		  </div>
+		  <div class="four columns">
+		    <h3 class="margined_left">Footer Navigation</h3>
+		    <ul class="margined_left">
+		    	<li><a href="">Company</a></li>
+		    	<li><a href="">Solutions</a></li>
+		    	<li><a href="">Products</a></li>
+		    	<li><a href="">Downloads</a></li>
+		    	<li><a href="">Support</a></li>
+		    </ul>
+		  </div>
+		  <div class="four columns">
+		    <h3>Latest News</h3>
+			<div class="footer_news_item clearfix">
+				<div class="footer_pic">
+					<a href=""><img src="images/footer_th1.jpg"/></a>
+				</div>
+				<h4><a href="">Those who mind don't matter, and vice-versa.</a></h4>
+				<p class="date">8 Sept., 2012</p>
+			</div>
+			<div class="footer_news_item clearfix">
+				<div class="footer_pic">
+					<a href=""><img src="images/footer_th2.jpg"/></a>
+				</div>
+				<h4><a href="">Be yourself; everyone else is already taken.</a></h4>
+				<p class="date">2 Sept., 2012</p>
+			</div>
+		  </div>
+
+		  <div class="four columns">
+		  	<h3>Get in Touch with Us</h3>
+		  	<div class='icon_phone' title='Phone'>044- 42616446, 24710701</div>
+		    <div class='icon_mail' title='Email'>biometric@in4solution.com</div>	
+		    <div class='icon_loc' title='Location'>No.25, 1st Floor,<br>Anna Main Road,<br>M.G.R Nagar,(K.K Nagar),<br>Chennai-600078,<br>Tamil Nadu.</div>	
+		    
+		  	<div class='clear'></div>
+		  	<div class='h10'></div>
+		
+		  </div> 
+	  </div> 
+	  <div class="clear"></div>
+	  <div class="footer_btm">
+	  	<div class="footer_btm_inner">
+	      	<a href='http://www.twitter.com' target='_blank' class='icon_tweet' title='Tweeter'>Tweeter</a>
+			<a href="http://www.skype.com" class='icon_skype' title='Skype'>Skype</a>  	
+		  	<a href='http://www.google.com' target='_blank' class='icon_google' title='Google+'>Google+</a>
+	      	<a href='http://www.facebook.com' target='_blank' class='icon_facebook' title='Facebook'>Facebook</a>  
+		  	<div id="powered"><a href="http://mitfsolutions.com" target="_blank">MITF Solutions</a> &copy; 2013
+		</div>	  
+	  </div>
+	</div>
+	<!-- Footer::END -->
+	 
+	
+  </div>
+
+</body>
+</html>
